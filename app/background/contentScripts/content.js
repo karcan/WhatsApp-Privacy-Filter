41bafb82c439b2d5224d2e15f97c9919b819f352
Karcan, Ozbal <karcanozbal@hotmail.com.tr>
2022-09-13 09:20:42 +0300

    some config added

--- app/background/contentScripts/content.js
+++ app/background/contentScripts/content.js
@@ -1,53 +1,54 @@
 function scriptFromFile(file, style = null) {
-	if(style){
-		var script = document.createElement("link");
-		script.href = chrome.runtime.getURL(file);
-		script.type = "text/css";
-		script.rel = "stylesheet";
+    if (style) {
+        var script = document.createElement("link");
+        script.href = chrome.runtime.getURL(file);
+        script.type = "text/css";
+        script.rel = "stylesheet";
 
-	}else{
-		var script = document.createElement("script");
-		script.src = chrome.runtime.getURL(file);
-	}
-	return script;
+    } else {
+        var script = document.createElement("script");
+        script.src = chrome.runtime.getURL(file);
+    }
+    return script;
 }
 
 function scriptFromSource(source, style = null) {
-	if(style){
-		var script = document.createElement("style");
-		script.textContent = source;
-	}else{
-		var script = document.createElement("script");
-		script.textContent = source;
-	}
-		
+    if (style) {
+        var script = document.createElement("style");
+        script.textContent = source;
+    } else {
+        var script = document.createElement("script");
+        script.textContent = source;
+    }
 
-	return script;
+
+    return script;
 }
 
 function inject(scripts) {
-	if (scripts.length === 0)
-		return;
+    if (scripts.length === 0)
+        return;
 
-	var otherScripts = scripts.slice(1);
-	var script = scripts[0];
+    var otherScripts = scripts.slice(1);
+    var script = scripts[0];
 
-	var onload = function() {
-		//script.parentNode.removeChild(script);
-		inject(otherScripts);
-	};
-	if (script.src != "") {
-		script.onload = onload;
-		document.head.appendChild(script);
-	} else {
-		document.head.appendChild(script);
-		onload();
-	}
+    var onload = function () {
+        //script.parentNode.removeChild(script);
+        inject(otherScripts);
+    };
+    if (script.src != "") {
+        script.onload = onload;
+        document.head.appendChild(script);
+    } else {
+        document.head.appendChild(script);
+        onload();
+    }
 }
 
 console.log('inject started..');
 inject([
-    scriptFromFile("app/public/js/jquery-3.5.1.min.js"),
+    //scriptFromFile("app/public/js/jquery-3.5.1.min.js"),
+    scriptFromFile("app/background/contentStyles/content.css", true),
     scriptFromFile("app/background/contentScripts/lib.js")
 ]);
 
--- app/background/contentScripts/lib.js
+++ app/background/contentScripts/lib.js
@@ -1,1 +1,27 @@
-console.log("WhatsApp Privacy Filter Loaded...");
+let whatsAppPrivacyFilter;
+
+whatsAppPrivacyFilter = {
+    init: () => {
+
+    },
+    options: {
+        "privacy:isActive": true,
+
+        "privacy:leftPane:recentBox:blurPx": 3.6,
+        "privacy:leftPane:recentBox:header:BlurPx": 0,
+        "privacy:leftPane:recentBox:avatar:BlurPx": 0,
+        "privacy:leftPane:recentBox:lastMessageTime:BlurPx": 0,
+        "privacy:leftPane:recentBox:lastMessageSender:BlurPx": 0,
+        "privacy:leftPane:recentBox:lastMessageContent:BlurPx": 0,
+        "privacy:leftPane:recentBox:lastMessageStatus:BlurPx": 0,
+
+
+        "privacy:leftPane:chatBox:avatar": 5,
+        "privacy:leftPane:chatBox:subTitle:blurPx": 3.6,
+        "privacy:leftPane:chatBox:title:blurPx": 3.6,
+        "privacy:leftPane:chatBox:messageBox:blurPx": 3.0,
+        "privacy:leftPane:chatBox:messageBox:image:blurPx": 15.0
+    }
+}
+
+whatsAppPrivacyFilter.init();
